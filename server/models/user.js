0b9bcafbc1b26f38186cfa8f5752c4c6c6e65b66
abubakrali10 <bakrali175@yahoo.com>
2024-03-01 03:37:35 +0200

    add time for created user

--- server/models/user.js
+++ server/models/user.js
@@ -30,7 +30,11 @@ const user = new mongoose.Schema({
             type: String,
             required: true,
         },
-    }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 }); 
 
 export const User = mongoose.model("User", user);
