9e18a23f3c6a7869d5c527800817e15017ff7d07
abubakrali10 <bakrali175@yahoo.com>
2024-03-01 15:15:12 +0200

    add imports

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ import cors from 'cors';
 import { dbConnection } from './database/dbConnection.js';
 import fileUpload from 'express-fileupload';
 import { errorMidware } from './middlewares/errorHandler.js';
-import userRouter from './routes/userRouter.js';  
+import userRouter from './routes/userRouter.js';
+import taskRouter from "./routes/taskRouter.js";
+
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
@@ -28,6 +30,8 @@ app.use(fileUpload({
 app.use(errorMidware);
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/task", taskRouter);
+
 
 dbConnection();
 
